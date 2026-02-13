052539fcb2ea39f7d9b068a00c5305d997c08492
Nayeem0011 <sknayeemislam384@gmail.com>
2026-02-14 00:30:04 +0600

    pageone section added

--- app/component/Pageone.js
+++ app/component/Pageone.js
@@ -1,16 +1,34 @@
 'use client';
 
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
-// import Image from 'next/image';
-// import stats_image from './image/stats_image.svg';
+import Marquee from "react-fast-marquee";
+import Image from "next/image";
+
+import company_logo1 from "./image/company_logo1.png";
+import company_logo2 from "./image/company_logo2.png";
+import company_logo3 from "./image/company_logo3.png";
+import company_logo4 from "./image/company_logo4.png";
 
 const Pageone = () => {
 
   return (
-    <section className="relative w-full h-screen overflow-hidden">
-
+    <section>
+      <div className="">
+        <p className="text-[#050E14] font-medium text-[18px] leading-[160%] tracking-[0%] text-center">
+          Trusted by leading organizations around the world
+        </p>
+      </div>
+      <div className="w-210.75 h-10 flex items-center gap-10 mx-auto mt-8 mb-4">
+        <Marquee speed={60} gradient={false} autoFill={false}>
+          <div className="flex items-center justify-center gap-10">
+            <Image src={company_logo1} alt="company_logo1" className="h-10 w-25.75" />
+            <Image src={company_logo2} alt="company_logo2" className="h-10 w-25.75" />
+            <Image src={company_logo3} alt="company_logo3" className="h-10 w-25.75" />
+            <Image src={company_logo4} alt="company_logo4" className="h-10 w-25.75" />
+            <Image src={company_logo4} alt="company_logo4" className="h-10 w-25.75" />
+            <Image src={company_logo4} alt="company_logo4" className="h-10 w-25.75" />
+          </div>
+        </Marquee>
+      </div>
     </section>
   );
 };
