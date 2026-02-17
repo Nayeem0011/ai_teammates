012d3585a94612ac21947830f63a6936c245deb5
Nayeem0011 <sknayeemislam384@gmail.com>
2026-02-18 00:47:27 +0600

    pagethree section added

--- app/component/Pagethree.js
+++ app/component/Pagethree.js
@@ -1,19 +1,22 @@
 'use client';
 
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import 'swiper/css';
-import 'swiper/css/navigation';
-gsap.registerPlugin(ScrollTrigger);
 // import Image from 'next/image';
 // import image48 from './image/image48.svg';
 
 const Pagethree = () => {
 
   return (
-    <div className="">
+    <section className="w-full mb-20">
+      <div className="flex items-center justify-center gap-8">
+        <div className="w-166.75 h-125 rounded-[19.83px] bg-white shadow-[0px_10.48px_10.48px_0px_#00000017]">
 
-    </div>
+        </div>
+
+        <div className="w-125.75 h-125 rounded-[19.83px] bg-white shadow-[0px_10.48px_10.48px_0px_#00000017]">
+
+        </div>
+      </div>
+    </section>
   );
 };
 
