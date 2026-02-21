9c74c7a32e7661bfbefbdd3363ddcba4977b7e19
Nayeem0011 <sknayeemislam384@gmail.com>
2026-02-22 01:20:29 +0600

    pagethree section added

--- app/component/Pagethree.js
+++ app/component/Pagethree.js
@@ -22,33 +22,28 @@ const Pagethree = () => {
 
         <div className="relative w-166.75 h-125 rounded-[19.83px] bg-white shadow-[0px_10.48px_10.48px_0px_#00000017] overflow-hidden">
           <div className='flex flex-col'>
-
-            {/* LEFT vertical slider indicator */}
-            <div className="absolute left-5 flex flex-col gap-3 z-20">
-              {slides.map((_, i) => (
-                <button
-                  key={i}
-                  onClick={() => setActive(i)}
-                  className={`rounded-full transition-all duration-300 ${active === i
-                    ? 'w-2.5 h-16.75 bg-blue-600'
-                    : 'w-2.5 h-2.5 bg-[#D9D9D9]'
-                    }`}
-                />
-              ))}
-            </div>
-
             {/* Card Stack Container */}
-            <div className="relative flex justify-center w-full h-full">
+            <div className="relative flex items-center justify-center w-full h-full pt-10">
+              {/* LEFT vertical slider indicator */}
+              <div className="absolute left-5 flex flex-col gap-3 z-20">
+                {slides.map((_, i) => (
+                  <button
+                    key={i}
+                    onClick={() => setActive(i)}
+                    className={`rounded-full transition-all duration-300 ${active === i
+                      ? 'w-2.5 h-16.75 bg-blue-600'
+                      : 'w-2.5 h-2.5 bg-[#D9D9D9]'
+                      }`}
+                  />
+                ))}
+              </div>
 
               {/* Inner stack wrapper */}
               <div className="relative h-74 w-136.25 flex items-center justify-center">
-
                 {/* Back card 2 */}
                 <div className="absolute h-74 w-136.25 rounded-[30px] bg-white opacity-30 scale-95 translate-y-6 shadow-lg" />
-
                 {/* Back card 1 */}
                 <div className="absolute h-74 w-136.25 rounded-[30px] bg-white opacity-60 scale-97 translate-y-3 shadow-lg" />
-
                 {/* Main Card */}
                 <div className="relative h-74 w-136.25 rounded-[30px] bg-white overflow-hidden">
                   <Image
@@ -82,14 +77,14 @@ const Pagethree = () => {
               </div>
             </div>
 
-            <div className=''>
-            <h1 className='text-[#050E14] font-medium text-[24px] leading-[115%] tracking-[-1%]'>
-              Seamless Integrations
-            </h1>
-            <p className='text-[#4B4F57] font-normal text-[16px] leading-[160%] tracking-[0%] pt-3'>
-              Deliver natural, context-aware responses across every channel, ensuring a <br /> personal touch in every conversation.
-            </p>
-          </div>
+            <div className='pt-10 pl-8'>
+              <h1 className='text-[#050E14] font-medium text-[24px] leading-[115%] tracking-[-1%]'>
+                Seamless Integrations
+              </h1>
+              <p className='text-[#4B4F57] font-normal text-[16px] leading-[160%] tracking-[0%] pt-3'>
+                Deliver natural, context-aware responses across every channel, ensuring a <br /> personal touch in every conversation.
+              </p>
+            </div>
           </div>
         </div>
 
